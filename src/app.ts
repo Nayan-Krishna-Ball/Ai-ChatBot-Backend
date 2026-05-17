2ebaf878885669f7435005bebfb537256d9fee71
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-05-18 00:21:42 +0600

    Refactor user authentication: implement user login and enhance signup flow

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 //
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 // import cors from "cors";
 import { config } from "dotenv";
 import express from "express";
@@ -12,7 +12,7 @@ const app = express();
 //middlewares
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-// app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //remove it in production
 app.use(morgan("dev"));
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,14 +1,23 @@
 //
 
 import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import {
+  getAllUsers,
+  userLogin,
+  userSignup,
+} from "../controllers/user-controllers.js";
+import {
+  loginValidator,
+  signupValidator,
+  validate,
+} from "../utils/validators.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
-userRoutes.post("/signup", userSignup);
+userRoutes.post("/signup", validate(signupValidator), userSignup);
+userRoutes.post("/login", validate(loginValidator), userLogin);
 
-// userRoutes.post("/login", validate(loginValidator), userLogin);
 // userRoutes.get("/auth-status", verifyToken, verifyUser);
 // userRoutes.get("/logout", verifyToken, userLogout);
 
